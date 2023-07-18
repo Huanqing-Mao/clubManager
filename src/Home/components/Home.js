d0190e6f5d44cf1f1428a794aebce6c25f003034
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2023-07-18 20:16:24 +0800

    Update Home.js
    Added alert to guide users on how to go switch to other CCA pages

--- src/Home/components/Home.js
+++ src/Home/components/Home.js
@@ -1,4 +1,4 @@
-import { Divider, Card } from "antd";
+import { Divider, Card, Alert } from "antd";
 import Timeline from "./Timeline";
 import AnnMain from "./Announcements/AnnMain";
 
@@ -19,34 +19,44 @@ function Home({
   }
 
   return (
-    <div className="flex-container">
-      <div className="calendar">
-        <Card
-          style={{
-            height: 70,
-            fontSize: 18,
-            backgroundColor: "rgba(7, 133, 165, 0.1)"
-          }}
-        >
-          <div className="timeline">Event Timeline</div>
-        </Card>
-        <p></p>
-        <Timeline currentID={userID} ccaID={ccaID} manager={manager} />
-      </div>
-      <div className="AnnSection">
-        <Card
-          style={{
-            height: 70,
-            fontSize: 18,
-            backgroundColor: "rgba(7, 133, 165, 0.1)"
-          }}
-        >
-          <div onClick={goAnn} className="announcement">
-            Recent Announcements
-            <p></p>
-          </div>
-        </Card>
-        <AnnMain userID={userID} ccaID={ccaID} manager={manager} />
+    <div>
+      <p></p>
+      <Alert
+        message="Navigation to other CCA Pages"
+        description="Please go to Profile >> Switch CCA to access other CCA Pages."
+        type="info"
+        showIcon
+        closable
+      />
+      <div className="flex-container">
+        <div className="calendar">
+          <Card
+            style={{
+              height: 70,
+              fontSize: 18,
+              backgroundColor: "rgba(7, 133, 165, 0.1)"
+            }}
+          >
+            <div className="timeline">Event Timeline</div>
+          </Card>
+          <p></p>
+          <Timeline currentID={userID} ccaID={ccaID} manager={manager} />
+        </div>
+        <div className="AnnSection">
+          <Card
+            style={{
+              height: 70,
+              fontSize: 18,
+              backgroundColor: "rgba(7, 133, 165, 0.1)"
+            }}
+          >
+            <div onClick={goAnn} className="announcement">
+              Recent Announcements
+              <p></p>
+            </div>
+          </Card>
+          <AnnMain userID={userID} ccaID={ccaID} manager={manager} />
+        </div>
       </div>
     </div>
   );

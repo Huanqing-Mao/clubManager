de545d65f96566059dbf41fabe861f1f011ec514
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2023-07-20 10:02:16 +0800

    Fixing Profile Updates MS3
    Users' existing information will be displayed at the update information page.

--- src/Home/components/SelectFaculty.js
+++ src/Home/components/SelectFaculty.js
@@ -1,8 +1,11 @@
 import { Select, Space } from "antd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function SelectFaculty({ setFaculty }) {
-  const [defaultValue, setDefault] = useState("Select Faculty");
+export default function SelectFaculty({ defaultFac, setFaculty }) {
+  const [defaultValue, setDefault] = useState(defaultFac);
+  useEffect(() => {
+    setDefault(defaultFac);
+  }, [defaultFac]);
   const handleChange = (value) => {
     setDefault(value);
     console.log(`selected ${value}`);
@@ -15,7 +18,7 @@ export default function SelectFaculty({ setFaculty }) {
   const SelectMenu = () => (
     <Space wrap>
       <Select
-        defaultValue={defaultValue}
+        value={defaultValue}
         style={{
           width: 250
         }}
